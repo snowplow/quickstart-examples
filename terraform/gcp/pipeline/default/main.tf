ef53ee2a7e5325c2ac9195617bf7e61a74b0562e
jbeemster <joshua.a.beemster@gmail.com>
2021-08-31 14:04:56 +0200

    Add GCP Pipeline for 21.08 release (closes #15)

---
+++ terraform/gcp/pipeline/default/main.tf
@@ -0,0 +1,189 @@
+locals {
+  custom_iglu_resolvers = [
+    {
+      name            = "Iglu Server"
+      priority        = 0
+      uri             = "${var.iglu_server_dns_name}/api"
+      api_key         = var.iglu_super_api_key
+      vendor_prefixes = []
+    }
+  ]
+}
+
+# 1. Deploy PubSub Topics
+module "raw_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
+
+  name = "${var.prefix}-raw-topic"
+
+  labels = var.labels
+}
+
+module "bad_1_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
+
+  name = "${var.prefix}-bad-1-topic"
+
+  labels = var.labels
+}
+
+module "enriched_topic" {
+  source  = "snowplow-devops/pubsub-topic/google"
+  version = "0.1.0"
+
+  name = "${var.prefix}-enriched-topic"
+
+  labels = var.labels
+}
+
+# 2. Deploy Collector stack
+module "collector_pubsub" {
+  source  = "snowplow-devops/collector-pubsub-ce/google"
+  version = "0.2.0"
+
+  name = "${var.prefix}-collector-server"
+
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
+
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
+
+  topic_project_id = var.project_id
+  good_topic_name  = module.raw_topic.name
+  bad_topic_name   = module.bad_1_topic.name
+
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
+
+  labels = var.labels
+}
+
+module "collector_lb" {
+  source  = "snowplow-devops/lb/google"
+  version = "0.1.0"
+
+  name = "${var.prefix}-collector-lb"
+
+  instance_group_named_port_http = module.collector_pubsub.named_port_http
+  instance_group_url             = module.collector_pubsub.instance_group_url
+  health_check_self_link         = module.collector_pubsub.health_check_self_link
+
+  ssl_certificate_enabled = var.ssl_information.enabled
+  ssl_certificate_id      = var.ssl_information.certificate_id
+}
+
+# 3. Deploy Enrichment
+module "enrich_pubsub" {
+  source  = "snowplow-devops/enrich-pubsub-ce/google"
+  version = "0.1.0"
+
+  name = "${var.prefix}-enrich-server"
+
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
+
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
+
+  raw_topic_name = module.raw_topic.name
+  good_topic_id  = module.enriched_topic.id
+  bad_topic_id   = module.bad_1_topic.id
+
+  # Linking in the custom Iglu Server here
+  custom_iglu_resolvers = local.custom_iglu_resolvers
+
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
+
+  labels = var.labels
+}
+
+# 4. Deploy Postgres Loader
+module "pipeline_db" {
+  source  = "snowplow-devops/cloud-sql/google"
+  version = "0.1.0"
+
+  name = "${var.prefix}-pipeline-db"
+
+  region      = var.region
+  db_name     = var.pipeline_db_name
+  db_username = var.pipeline_db_username
+  db_password = var.pipeline_db_password
+
+  authorized_networks = var.pipeline_db_authorized_networks
+
+  tier = var.pipeline_db_tier
+
+  labels = var.labels
+}
+
+module "postgres_loader_enriched" {
+  source  = "snowplow-devops/postgres-loader-pubsub-ce/google"
+  version = "0.2.0"
+
+  name = "${var.prefix}-pg-loader-enriched-server"
+
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
+  project_id = var.project_id
+
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
+
+  in_topic_name = module.enriched_topic.name
+  purpose       = "ENRICHED_EVENTS"
+  schema_name   = "atomic"
+
+  db_instance_name = module.pipeline_db.connection_name
+  db_port          = module.pipeline_db.port
+  db_name          = var.pipeline_db_name
+  db_username      = var.pipeline_db_username
+  db_password      = var.pipeline_db_password
+
+  # Linking in the custom Iglu Server here
+  custom_iglu_resolvers = local.custom_iglu_resolvers
+
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
+
+  labels = var.labels
+}
+
+module "postgres_loader_bad" {
+  source  = "snowplow-devops/postgres-loader-pubsub-ce/google"
+  version = "0.2.0"
+
+  name = "${var.prefix}-pg-loader-bad-server"
+
+  network    = var.network
+  subnetwork = var.subnetwork
+  region     = var.region
+  project_id = var.project_id
+
+  ssh_ip_allowlist = var.ssh_ip_allowlist
+  ssh_key_pairs    = var.ssh_key_pairs
+
+  in_topic_name = module.bad_1_topic.name
+  purpose       = "JSON"
+  schema_name   = "atomic_bad"
+
+  db_instance_name = module.pipeline_db.connection_name
+  db_port          = module.pipeline_db.port
+  db_name          = var.pipeline_db_name
+  db_username      = var.pipeline_db_username
+  db_password      = var.pipeline_db_password
+
+  # Linking in the custom Iglu Server here
+  custom_iglu_resolvers = local.custom_iglu_resolvers
+
+  telemetry_enabled = var.telemetry_enabled
+  user_provided_id  = var.user_provided_id
+
+  labels = var.labels
+}
---
+++ terraform/gcp/pipeline/default/terraform.tfvars
@@ -0,0 +1,81 @@
+# Will be prefixed to all resource names
+# Use this to easily identify the resources created and provide entropy for subsequent environments
+prefix = "sp"
+
+# The project to deploy the infrastructure into
+project_id = "PROJECT_ID_TO_DEPLOY_INTO"
+
+# Where to deploy the infrastructure
+region = "REGION_TO_DEPLOY_INTO"
+
+# --- Default Network
+# Update to the network you would like to deploy into
+#
+# Note: If you opt to use your own network then you will need to define a subnetwork to deploy into as well
+network    = "default"
+subnetwork = ""
+
+# --- SSH
+# Update this to your IP Address
+ssh_ip_allowlist = ["999.999.999.999/32"]
+# Generate a new SSH key locally with `ssh-keygen`
+# ssh-keygen -t rsa -b 4096 
+ssh_key_pairs = [
+  {
+    user_name  = "snowplow"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQA0jSi9//bRsHW4M6czodTs6smCXsxZ0gijzth0aBmycE= snowplow@Snowplows-MacBook-Pro.local"
+  }
+]
+
+# --- Iglu Server Configuration
+# Iglu Server DNS output from the Iglu Server stack
+iglu_server_dns_name = "http://CHANGE-TO-MY-IGLU-IP"
+# Used for API actions on the Iglu Server
+# Change this to the same UUID from when you created the Iglu Server
+iglu_super_api_key = "00000000-0000-0000-0000-000000000000"
+
+# NOTE: Needed for Postgres Loader to work
+# igluctl: https://docs.snowplowanalytics.com/docs/pipeline-components-and-applications/iglu/igluctl
+# Ensure you have seeded Iglu Server: 
+# git clone https://github.com/snowplow/iglu-central
+# cd iglu-central
+# igluctl static push --public schemas/ http://CHANGE-TO-MY-IGLU-URL.elb.amazonaws.com 00000000-0000-0000-0000-000000000000
+
+# --- Snowplow Postgres Loader
+pipeline_db_name     = "snowplow"
+pipeline_db_username = "snowplow"
+# Change and keep this secret!
+pipeline_db_password = "Hell0W0rld!2"
+# IP ranges that you want to query the Pipeline Postgres Cloud SQL instance from directly over the internet.  An alternative access method is to leverage
+# the Cloud SQL Proxy service which creates an IAM authenticated tunnel to the instance
+#
+# Details: https://cloud.google.com/sql/docs/postgres/sql-proxy
+#
+# Note: this exposes your data to the internet - take care to ensure your allowlist is strict enough
+pipeline_db_authorized_networks = [
+  {
+    name  = "foo"
+    value = "999.999.999.999/32"
+  },
+  {
+    name  = "bar"
+    value = "888.888.888.888/32"
+  }
+]
+# Note: the size of the database instance determines the number of concurrent connections - each Postgres Loader instance creates 10 open connections so having
+# a sufficiently powerful database tier is important to not running out of connection slots
+pipeline_db_tier = "db-g1-small"
+
+# See for more information: https://registry.terraform.io/modules/snowplow-devops/collector-pubsub-ce/google/latest#telemetry
+# Telemetry principles: https://docs.snowplowanalytics.com/docs/open-source-quick-start/what-is-the-quick-start-for-open-source/telemetry-principles/
+user_provided_id  = ""
+telemetry_enabled = true
+
+# --- SSL Configuration (optional)
+ssl_information = {
+  certificate_id = ""
+  enabled        = false
+}
+
+# --- Extra Labels to append to created resources (optional)
+labels = {}
