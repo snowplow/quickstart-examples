0c5355592468cdb1f3b6c061f7f7025d55acf154
Paul Boocock <paul@snowplowanalytics.com>
2021-10-11 16:25:25 +0100

    Bump GCP CE based modules to latest (close #20)

--- terraform/gcp/pipeline/secure/main.tf
+++ terraform/gcp/pipeline/secure/main.tf
@@ -46,7 +46,7 @@ module "enriched_topic" {
 # 2. Deploy Collector stack
 module "collector_pubsub" {
   source  = "snowplow-devops/collector-pubsub-ce/google"
-  version = "0.2.0"
+  version = "0.2.1"
 
   name = "${var.prefix}-collector-server"
 
@@ -86,7 +86,7 @@ module "collector_lb" {
 # 3. Deploy Enrichment
 module "enrich_pubsub" {
   source  = "snowplow-devops/enrich-pubsub-ce/google"
-  version = "0.1.0"
+  version = "0.1.1"
 
   name = "${var.prefix}-enrich-server"
 
@@ -133,7 +133,7 @@ module "pipeline_db" {
 
 module "postgres_loader_enriched" {
   source  = "snowplow-devops/postgres-loader-pubsub-ce/google"
-  version = "0.2.0"
+  version = "0.2.1"
 
   name = "${var.prefix}-pg-loader-enriched-server"
 
@@ -168,7 +168,7 @@ module "postgres_loader_enriched" {
 
 module "postgres_loader_bad" {
   source  = "snowplow-devops/postgres-loader-pubsub-ce/google"
-  version = "0.2.0"
+  version = "0.2.1"
 
   name = "${var.prefix}-pg-loader-bad-server"
 
