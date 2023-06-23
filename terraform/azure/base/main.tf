32c5bc784d3bbc9fb5ed8ad790764faefb301b99
jbeemster <joshua.a.beemster@gmail.com>
2023-06-23 10:32:33 +1000

    Add azure/base module to create vnet and resource group - WIP

---
+++ terraform/azure/base/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+module "vnet" {
+  source  = "snowplow-devops/vnet/azurerm"
+  version = "0.1.1"
+
+  name                = "${var.prefix}-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = var.tags
+
+  depends_on = [azurerm_resource_group.rg]
+}
---
+++ terraform/azure/base/terraform.tfvars
@@ -0,0 +1,9 @@
+# Will be prefixed to all resource names
+# Use this to easily identify the resources created and provide entropy for subsequent environments
+prefix = "sp"
+
+# The location to create resources in: https://azure.microsoft.com/en-au/explore/global-infrastructure/geographies/#overview
+location = "<ADD_ME>"
+
+# Extra Tags to append to created resources (optional)
+tags = {}
